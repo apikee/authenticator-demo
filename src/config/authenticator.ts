f50fd6f7c6c04675201a375dbdfdfb5a24715b3c
Jakub Tomáš <jakubtomas108@gmail.com>
2022-07-15 22:03:59 +0200

    Add custom reject handler

--- src/config/authenticator.ts
+++ src/config/authenticator.ts
@@ -5,4 +5,9 @@ export const { createAccess, validateAccess, refreshAccess, revokeAccess } =
   new Authenticator({
     accessKey: environment.accessSecretKey,
     refreshKey: environment.refreshSecretKey,
+    rejectedAccessHandler: (req, res, next) => {
+      return res
+        .status(401)
+        .json({ message: "You are not authorized to perform this action" });
+    },
   });
